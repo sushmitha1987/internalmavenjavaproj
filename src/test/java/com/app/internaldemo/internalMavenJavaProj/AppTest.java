3531aee52ab6224d33522db4a11cd6a2c4bdc34a
sushmitha <sushmithachary369@gmail.com>
2025-01-02 06:17:11 +0530

    first commit of internaljavaproj

---
+++ src/test/java/com/app/internaldemo/internalMavenJavaProj/AppTest.java
@@ -0,0 +1,20 @@
+package com.app.internaldemo.internalMavenJavaProj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
